f89f5ac493b5807a294c5a898f114362334253d6
brentrug <b.rentrug@gmail.com>
2014-01-10 23:40:29 +0100

    cart

--- webshop/cart.php
+++ webshop/cart.php
@@ -1,20 +1,31 @@
-
 <?php
 if (!isset($_SESSION["warenkorb"])) {
 	$_SESSION["warenkorb"] = array();
 }
 
+//warenkorb aktualisieren
 if(isset($_POST["refresh"])) {
 
-	foreach($_POST['anzahl'] as $felder) {
-		echo $felder;
+
+	for($i=0;$i<=count($_POST["album_id"])-1; $i++) {
+		
+		$_SESSION["warenkorb"][$_POST["album_id"][$i]]["anzahl"] = $_POST["anzahl"][$i];
+		
+		if(isset($_POST["chk_digital"][$i])) {
+			$_SESSION["warenkorb"][$_POST["album_id"][$i]]["digital"] = $_POST["chk_digital"][$i];
+		} else {
+			$_SESSION["warenkorb"][$_POST["album_id"][$i]]["digital"] = "";
+		}
+	
 	}
 
 }
  
 
 //warenkorb leeren
-//unset($_SESSION["warenkorb"]);
+if(isset($_GET["clear"])) {
+	unset($_SESSION["warenkorb"]);
+}
 
 //print_r($_SESSION["warenkorb"]);
 
@@ -61,12 +72,14 @@ if(!empty($_SESSION["warenkorb"])) {
 	if ($result = $mysql->query($query)) {
 	
 		$platte = $result->fetch_object();
-		$preis = $platte->Price*$value["anzahl"];
+		$preis = ((int)$platte->Price)*(int)$value["anzahl"];
 		
 			?>
 				<li>
 					<div class="platte" stlye="width:100%;">
-						<input type="hidden" name="album_id" value="<? echo $value["album_id"];?>">
+						
+						<input type="hidden" name="album_id[]" value="<? echo $value["album_id"];?>">
+						
 							<img class="album_cover" alt="<? echo $platte->Album ?>"
 							src="Resources/Covers/<? echo $platte->CoverName ?>" />
 						<div class="album_info" stlye="display:inline-table;width:100%;">
@@ -74,10 +87,9 @@ if(!empty($_SESSION["warenkorb"])) {
 									<? echo $platte->Artist ." - ". $platte->Album ?>
 							</h4>
 							<span class="album_details">Anzahl <input type="number" value="<? echo $value["anzahl"];?>" name="anzahl[]"></span>
+							<span class="album_details">Digitaler Download? <input type="checkbox" name="chk_digital[]" <? if($value["digital"]=='on') { echo ' checked';}?> /></span>
 							
-							<span class="album_details">Digitaler Download? <input type="checkbox" name="digital[]"></span>
-							
-							<span class="album_details"> <? echo $platte->Price*$value["anzahl"];?> CHF</span>
+							<span class="album_details"> <? echo $preis;?> CHF</span>
 							
 							<span class="album_details"> <a href="?site=cart&item=<? echo $value["album_id"];?>&del">X</a></span>
 						</div>
@@ -93,8 +105,7 @@ if(!empty($_SESSION["warenkorb"])) {
 	
 	}
 
-	echo '<li>Total: '.$tot_price.' CHF</li> <input type="submit" name="refresh" value="Aktualisieren" />
-</form><ul>';
+	echo '<li>Total: '.$tot_price.' CHF</li> <input type="submit" name="refresh" value="Aktualisieren" /> <a href="?site=cart&clear"> Warenkorb leeren</a></form><ul>';
 	
 } else {
 	echo "Der Warenkorb ist leer";
