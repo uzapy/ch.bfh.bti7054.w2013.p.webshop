34cc47ab3c834329bf768289ec29f35e75cb5943
brentrug <b.rentrug@gmail.com>
2014-01-11 00:37:12 +0100

    pdf

---
+++ webshop/PDF/order.php
@@ -0,0 +1,49 @@
+<?php
+require('fpdf.php');
+
+session_start();
+
+include '../php/db_connection.php';
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+
+$pdf->Cell(80,10,'Plattelade - Bestellungen',0, 1);
+
+$pdf->SetFont('Arial','',12);
+
+$tot_price = 0;
+
+foreach ($_SESSION["warenkorb"] as $key => $value) {
+	
+	
+	if(isset($value["album_id"])) {
+		$query = "SELECT * FROM Platten WHERE ID = ".$value["album_id"];
+		
+		if ($result = $mysql->query($query)) {
+		
+			$platte = $result->fetch_object();
+			$preis = ((int)$platte->Price)*(int)$value["anzahl"];
+			
+			//$pdf->Image('../Resources/Covers/'.$platte->CoverName, 10, 20, 20, 20);
+			
+			$pdf->Cell(80,10,$value["anzahl"]."x ".$platte->Artist ." - ". $platte->Album." | ".$preis." CHF",0, 1);
+			
+			//total preis errechnen
+			$tot_price = $tot_price + $preis;
+			
+		}
+	}
+}
+
+
+$pdf->SetFont('Arial','B',12);
+
+$pdf->Cell(80,10,'',0, 1);
+
+$pdf->Cell(80,10,'Total: '.$tot_price.' CHF',0, 1);
+
+
+$pdf->Output();
+?>
--- webshop/cart.php
+++ webshop/cart.php
@@ -6,7 +6,6 @@ if (!isset($_SESSION["warenkorb"])) {
 //warenkorb aktualisieren
 if(isset($_POST["refresh"])) {
 
-
 	for($i=0;$i<=count($_POST["album_id"])-1; $i++) {
 		
 		$_SESSION["warenkorb"][$_POST["album_id"][$i]]["anzahl"] = $_POST["anzahl"][$i];
@@ -39,17 +38,22 @@ if(isset($_GET["item"])) {
 		$album["anzahl"] = 1;
 		$album["digital"] = "0";
 		
+		if(isset($_SESSION["warenkorb"][$album["album_id"]])) {
+			$w_id = $_SESSION["warenkorb"][$album["album_id"]];
+		}
+		
 		if(empty($_SESSION["warenkorb"])) {
 			// neues album in den warebkorb
 			$_SESSION["warenkorb"][$album["album_id"]] = $album;
-		} else {		
-			if($_SESSION["warenkorb"][$album["album_id"]]["album_id"] != $album["album_id"]) {
-				// neues album in den warebkorb
-				$_SESSION["warenkorb"][$album["album_id"]] = $album;
-			} else {
-				//bisheriges album um 1 erhöhen
-				$_SESSION["warenkorb"][$album["album_id"]]["anzahl"] = $_SESSION["warenkorb"][$album["album_id"]]["anzahl"] + 1;
-			}
+		} else {
+		
+				if($w_id["album_id"] != $album["album_id"]) {
+					// neues album in den warebkorb
+					$_SESSION["warenkorb"][$album["album_id"]] = $album;
+				} else {
+					//bisheriges album um 1 erhöhen
+					$_SESSION["warenkorb"][$album["album_id"]]["anzahl"] = $w_id["anzahl"] + 1;
+				}
 		}
 	} else {
 		// album entfernen
@@ -66,46 +70,46 @@ if(!empty($_SESSION["warenkorb"])) {
 	
 	foreach ($_SESSION["warenkorb"] as $key => $value) {
 	
+	if(isset($value["album_id"])) {
+		$query = "SELECT * FROM Platten WHERE ID = ".$value["album_id"];
 		
-	$query = "SELECT * FROM Platten WHERE ID = ".$value["album_id"];
-	
-	if ($result = $mysql->query($query)) {
-	
-		$platte = $result->fetch_object();
-		$preis = ((int)$platte->Price)*(int)$value["anzahl"];
+		if ($result = $mysql->query($query)) {
 		
-			?>
-				<li>
-					<div class="platte" stlye="width:100%;">
-						
-						<input type="hidden" name="album_id[]" value="<? echo $value["album_id"];?>">
-						
-							<img class="album_cover" alt="<? echo $platte->Album ?>"
-							src="Resources/Covers/<? echo $platte->CoverName ?>" />
-						<div class="album_info" stlye="display:inline-table;width:100%;">
-							<h4>
-									<? echo $platte->Artist ." - ". $platte->Album ?>
-							</h4>
-							<span class="album_details">Anzahl <input type="number" value="<? echo $value["anzahl"];?>" name="anzahl[]"></span>
-							<span class="album_details">Digitaler Download? <input type="checkbox" name="chk_digital[]" <? if($value["digital"]=='on') { echo ' checked';}?> /></span>
+			$platte = $result->fetch_object();
+			$preis = ((int)$platte->Price)*(int)$value["anzahl"];
+			
+				?>
+					<li>
+						<div class="platte">
 							
-							<span class="album_details"> <? echo $preis;?> CHF</span>
+							<input type="hidden" name="album_id[]" value="<? echo $value["album_id"];?>">
 							
-							<span class="album_details"> <a href="?site=cart&item=<? echo $value["album_id"];?>&del">X</a></span>
+								<img class="album_cover" alt="<? echo $platte->Album ?>"
+								src="Resources/Covers/<? echo $platte->CoverName ?>" />
+								
+							<div class="album_info"">
+								<h4>
+										<? echo $platte->Artist ." - ". $platte->Album ?>
+								</h4>
+								<span class="album_details">Anzahl <input type="number" value="<? echo $value["anzahl"];?>" name="anzahl[]"></span>
+								<span class="album_details">Digitaler Download? <input type="checkbox" name="chk_digital[]" <? if($value["digital"]=='on') { echo ' checked';}?> /></span>
+								
+								<span class="album_details"> <? echo $preis;?> CHF</span>
+								
+								<span class="album_details"> <a href="?site=cart&item=<? echo $value["album_id"];?>&del">X</a></span>
+							</div>
 						</div>
-					</div>
-				</li>
-		<?
-		
-		//total preis errechnen
-		$tot_price = $tot_price + $preis;
-		
+					</li>
+			<?
+			
+			//total preis errechnen
+			$tot_price = $tot_price + $preis;
+			
+			}	
 		}
-		
-	
 	}
-
-	echo '<li>Total: '.$tot_price.' CHF</li> <input type="submit" name="refresh" value="Aktualisieren" /> <a href="?site=cart&clear"> Warenkorb leeren</a></form><ul>';
+	
+	echo '<li>Total: <b>'.$tot_price.' CHF</b></li> <input type="submit" name="refresh" value="Aktualisieren" /> <a href="?site=cart&clear"> Warenkorb leeren</a></form><a href="?site=checkout"> Bestellen</a><ul>';
 	
 } else {
 	echo "Der Warenkorb ist leer";
